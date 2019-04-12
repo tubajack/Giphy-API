640f494ddb2b5f338c50a5cc9becfc5efbaaec95
Jackson Henry <jackson.henry13@yahoo.com>
2019-04-12 14:22:48 -0500

    Am setting up the Ajax for the Javascript code.

--- giphyLogic.js
+++ giphyLogic.js
@@ -27,6 +27,14 @@ window.onload = function(){
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" 
         + musicalInstrument + "&api_key=ZoA453GaabwjKDDflogoSaFJnc6BOvd2&limit=10";
 
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+
+        }).then(function(response){
+
+        })
+
     });
 
 
